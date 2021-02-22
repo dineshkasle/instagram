8c247a3bfde121573c3f01ec982d31e14afe7279
Dinesh <dkasale@3cubetechmed.com>
2021-02-22 23:51:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostsComponent} from "./posts/posts.component";
+import {StoriesComponent} from "./stories/stories.component";
+import {HeaderComponent} from "./header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:PostsComponent},
+  {path:'posts', component:PostsComponent},
+  {path:'story', component:StoriesComponent},
+  {path:'headers', component:HeaderComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
