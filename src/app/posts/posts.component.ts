9877c8e269ca8cae5761c56646e0a553b432b25a
Dinesh <dkasale@3cubetechmed.com>
2021-02-26 21:15:11 +0530

    commit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  myPost = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.myPost = [{
+      imgUri: "https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg",
+      userName: 'Dinesh Kasale',
+      location: 'Mumbai, India',
+      mainImgUrl: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg',
+      likes: 1823,
+      likeClass:'fa fa-heart-o '
+    },
+      {
+        imgUri: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg",
+        userName: 'Danny Castle',
+        location: 'Delhi, India',
+        mainImgUrl: 'https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg',
+        likes: 2500,
+        likeClass:'fa fa-heart-o '
+      }];
+  }
+
+  likePost(index){
+    if(this.myPost[index].likeClass === 'fa fa-heart-o '){
+      this.myPost[index].likeClass = 'fa fa-heart red';
+    }else {
+      this.myPost[index].likeClass = 'fa fa-heart-o ';
+    }
+  }
+
+  likedPost(index){
+    if(this.myPost[index].likeClass === 'fa fa-heart-o '){
+      this.myPost[index].likeClass = 'fa fa-heart red';
+    }else {
+      this.myPost[index].likeClass = 'fa fa-heart-o ';
+    }
   }
 
 }
